18f3d47151e413fd1afada3f4d92f6ef42abdec2
Anita <anita@foundersandcoders.com>
2018-04-06 08:51:10 +0100

    Form (#2)
    * feat: add handlebars and create new endpoints
    
    * feat: ajax request from form
    
    * feat: create views

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import express from 'express';
 import http from 'http';
+import exphbs from 'express-handlebars';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.use(express.static(__dirname + '/public'))
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 8000);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/', (req, res) => res.render('home'));
+app.get('/join', (req, res) => res.render('join'));
+app.post('/subscribe', urlencodedParser, (req, res) => {
+  res.send({ res: 'ok' });
 });
 
 const server = http.createServer(app);
 
 server.listen(app.get('port'), () => {
-    console.log('listening on :' + app.get('port'));
+  console.log('listening on :' + app.get('port'));
 });
