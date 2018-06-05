c69b3266389ab8d15a9dc1440d853321af0dceb4
Anita <a.nita@hotmail.co.uk>
2018-06-05 21:11:25 +0100

    feat: sends email template on subscribe, working!

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
+'use strict'
+
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
+import subscribeHandler from './controllers/subscribe';
 
 const app = express();
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.enable('trust proxy');
+app.use(function(req, res, next) {
+  if (process.env.NODE_ENV === 'production') {
+    if (req.headers['x-forwarded-proto'] === 'https' || req.secure) return next();
+
+    return res.redirect(301, `https://${join(req.hostname, req.url)}`);
+  }
+  return next();
+});
 
 app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 8000);
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
-app.post('/subscribe', urlencodedParser, (req, res) => {
-  res.send({ res: 'ok' });
-});
+
+app.post('/subscribe', urlencodedParser, subscribeHandler);
 
 const server = http.createServer(app);
 
